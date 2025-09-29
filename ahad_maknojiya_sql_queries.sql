0ec8a5ade640e607596562893a39d8b1cac29d73
ahad-maknojiya <ahad.maknojiya@tamu.edu>
2025-09-29 18:46:43 -0500

    Update ahad_maknojiya_sql_queries.sql

--- ahad_maknojiya_sql_queries.sql
+++ ahad_maknojiya_sql_queries.sql
@@ -1,9 +1,9 @@
-Transaction per day
+--Transaction per day
 SELECT time::date AS day, COUNT(*) FROM transaction GROUP BY day ORDER by day;
 
-Money made per day
+--Money made per day
 SELECT time::date AS day, SUM(cost) FROM transaction GROUP BY day ORDER by day;
 
-Best Week
+--Best Week
 SELECT time::date AS week, SUM(cost) AS week_sum FROM transaction GROUP BY week ORDER BY week_sum DESC LIMIT 1;
 
