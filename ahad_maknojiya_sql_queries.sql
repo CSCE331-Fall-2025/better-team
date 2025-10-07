766ba584bfaccdef92a867acae55dfb01a63e806
Alex <charhp713@tamu.edu>
2025-10-07 12:56:22 -0500

    Delete ahad_maknojiya_sql_queries.sql
    clearing

--- ahad_maknojiya_sql_queries.sql
+++
@@ -1,9 +0,0 @@
---Transaction per day
-SELECT time::date AS day, COUNT(*) FROM transaction GROUP BY day ORDER by day;
-
---Money made per day
-SELECT time::date AS day, SUM(cost) FROM transaction GROUP BY day ORDER by day;
-
---Best Week
-SELECT time::date AS week, SUM(cost) AS week_sum FROM transaction GROUP BY week ORDER BY week_sum DESC LIMIT 1;
-
