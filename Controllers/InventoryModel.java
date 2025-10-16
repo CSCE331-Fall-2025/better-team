45b552ea4e14ed029a59281fd36464b38db2b0b6
ahad-maknojiya <ahad.maknojiya@tamu.edu>
2025-10-16 15:15:22 -0500

    updated inventory files

--- Controllers/InventoryModel.java
+++ Controllers/InventoryModel.java
@@ -1,18 +1,15 @@
-package controllers;
+// Handles all database operations (SQL queries) related to inventory and dishes.
+// This is your Data Access Object (DAO) layer.
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import Database.Item;
-
-import app.dbSetup;
-
 public class InventoryModel {
     private static final String DB_URL = "jdbc:postgresql://csce-315-db.engr.tamu.edu/CSCE315Database";
 
-    public List<Item> getInventoryItems() {
-        List<Item> items = new ArrayList<>();
+    public List<InventoryController.Item> getInventoryItems() {
+        List<InventoryController.Item> items = new ArrayList<>();
         try {
             dbSetup my = new dbSetup();
             Class.forName("org.postgresql.Driver");
@@ -26,8 +23,7 @@ public class InventoryModel {
                 String name = rs.getString("item");
                 int current = rs.getInt("current_inventory");
                 int recommended = rs.getInt("target_inventory");
-
-                items.add(new Item(name, current, recommended));
+                items.add(new InventoryController.Item(name, current, recommended));
             }
 
             rs.close();
